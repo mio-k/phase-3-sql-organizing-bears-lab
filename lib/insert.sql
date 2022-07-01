c278569bcfd572496c17e93ff743cb5a00f8bd03
mio kataoka <mio.kanna@gmail.com>
2022-07-01 16:32:56 -0700

    commit tree af70b6d2cd3ccd878a290c7442d9b2949a0b622d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears Values(1, "Mr. Chocolate", 3, "M", "Brown", "Chill", 1);
+INSERT INTO bears Values(2, "Rowdy", 3, "M", "Black", "Wild", 1);
+INSERT INTO bears Values(3,"Tabitha", 5, "F", "Brown", "Bossy", 1);
+INSERT INTO bears Values(4, "Sargeant Brown", 7, "F", "Brown", "Calm", 1);
+INSERT INTO bears Values(5, "Melissa", 1, "F", "Light Brown", "Smart", 1);
+INSERT INTO bears Values(6, "Grinch", 9, "M", "White", "Dark", 1);
+INSERT INTO bears Values(7, "Wendy", 6,"F", "Dark Brown", "Goofy", 1);
+INSERT INTO bears Values(8, null, 6, "F", "Dark Brown", "Goofy", 1);
